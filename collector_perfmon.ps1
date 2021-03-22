af173450507d019403f58144e2ed84eda86be89e
yanivetrogi <yaniv.etrogi@gmail.com>
2021-03-22 08:13:23 +0200

    New
    New

--- collector_perfmon.ps1
+++ collector_perfmon.ps1
@@ -1,53 +1,41 @@
-$ErrorActionPreference = 'Stop'
+Import-Module psgelf;
+#$ErrorActionPreference = 'Stop'
 #$path = Get-Location;
 
-if ($psISE) {$path = Split-Path -Path $psISE.CurrentFile.FullPath;} else {$path = $global:PSScriptRoot;}
+#if ($psISE) {$path = Split-Path -Path $psISE.CurrentFile.FullPath;} else {$path = $global:PSScriptRoot;}
 
-
-    $collector_name = $MyInvocation.MyCommand.Name.Split(".")[0];
-    
-    [string]$config_file_full_name = Join-Path $path 'config.json';
-    [PSCustomObject]$config_file = Get-Content  $config_file_full_name | Out-String| ConvertFrom-Json;
-
-
-    $processing_path = $config_file.processing_path
-    $ready_path = $config_file.ready_path
-    $customername = $config_file.customername
-    $metricspath = $config_file.metricspath
-
-    $server = get-content env:computername   
-     #"C:\GoogleDrive\Team\PowerShell\CloudMonitoring\new_collectors\helpers.ps1" 
-     Join-Path $path 'helpers.ps1';
    
-   <#
-    function writemetrictofile($objarr, $name)
-    {
-        $processing_path = $config_file.processing_path
-        $ready_path = $config_file.ready_path
-        $customername = $config_file.customername
-        $file = $customername +"-" + $name +"-" + $(Get-date -Format yyyyMMdd-HHmmssffff) +'.csv' 
-        $full_path = $processing_path + '\' + $file
-        $hostn = get-content env:computername 
-        
-        $now = Get-Date ([datetime]::UtcNow) -Format s
-        $now = $now.ToString()+"Z"        
+#region <variables>
+[string]$config_file_full_name = Join-Path $PSScriptRoot 'config.json';
+[PSCustomObject]$config_file = Get-Content  $config_file_full_name | Out-String| ConvertFrom-Json;
 
-        foreach ($obj in $objarr)
-        {            
-            $obj | Add-Member -MemberType NoteProperty -Name FormatedTimestamp -Value $now -Force
-            $obj | Add-Member -MemberType NoteProperty -Name Computer -Value $hostn.trim() -Force;
-        }
-        $objarr | Export-Csv -Path $full_path -NoTypeInformation;
-        $target_file = $ready_path + '\' + $file
-        Move-Item $full_path $target_file        
-    }
-    #>
+Join-Path $PSScriptRoot 'helpers.ps1';
+
+[bool]$user_interactive = [Environment]::UserInteractive;
+[string]$collector_name = $MyInvocation.MyCommand.Name.Split(".")[0];
+
+$processing_path = $config_file.processing_path
+$ready_path = $config_file.ready_path
+$customername = $config_file.customername
+$metricspath = $config_file.metricspath
+
+[string[]]$servers = $config_file.servers; 
+
+[string]$graylog_server = $config_file.graylog_server;
+[string]$graylog_port = $config_file.graylog_port;
+
+
+#endregion
+
+
     
-    $server = get-content env:computername 
-    $delay=1
-    $count=1
+    
+$delay=1;
+$count=1;
 
-
+foreach($server in $servers)
+{
+    if ($user_interactive -eq $true) {Write-Host -ForegroundColor Green $server }; 
     try
     {
         $metrics = Get-Counter -Counter $metricspath -SampleInterval $delay -MaxSamples $count            
@@ -56,15 +44,25 @@ if ($psISE) {$path = Split-Path -Path $psISE.CurrentFile.FullPath;} else {$path 
             $obj = $metric.CounterSamples | Select-Object -Property Path, CookedValue;   
             $objarr += $obj          
         }     
+        # Export to csv
+        #writemetrictofile ($objarr) ($collector_name)    
 
-    writemetrictofile ($objarr) ($collector_name)    
+
+        $obj = $null;
+        
+        # Graylog
+        foreach($obj in $objarr)
+        {    
+            if ($user_interactive -eq $true) {Write-Host -ForegroundColor Cyan $server $obj.Path $obj.CookedValue}; 
+            #Send-PSGelfUDP -GelfServer $graylog_server -Port $graylog_port -ShortMessage $obj.path -FullMessage $obj.CookedValue;
+        }
+    }
+    catch {Throw; }
 }
-catch {Throw; }
 
-#finally 
-#    {
-#        $objarr = $null; 
-#        $collector_name - $null;
-#        $obj = $null;
-#    }
+$metrics = $null;
+$objarr =  $null; 
+$collector_name = $null;
+$obj = $null;
+
 
